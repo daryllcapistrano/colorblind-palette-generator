beb7229eafc94377a0ce59276259dcba3635f2bb
Daryll <dcapistrano.dev@gmail.com>
2021-01-20 16:15:24 -0800

    refactor into own folder

---
+++ src/components/sections/footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box } from '@chakra-ui/react';
+
+function Footer() {
+  return (
+    <Box bg="gray.500" w="100%" p={4} color="gray.50">
+      Footer
+    </Box>
+  );
+}
+
+export default Footer;
